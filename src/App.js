38bbd0f712870fc7f5a4fa8aaee5091c1edf78a2
Rene Marino <renemarino93@gmail.com>
2021-12-21 23:46:47 -0800

    axios version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
 
   const [pokedex,setPokedex] = useState([]);
-  
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1118')
-        .then(response => response.json())
+// This was for the original PokeAPI
+//   useEffect(() => {
+//     fetch('https://pokeapi.co/api/v2/pokemon?limit=1118')
+//         .then(response => response.json())
 
-        .then(response => setPokedex(response.results))
+//         .then(response => setPokedex(response.results))
+// }, []);
+
+useEffect(()=>{
+  axios.get('https://pokeapi.co/api/v2/pokemon?limit=1118')
+      .then(response=>{setPokedex(response.data.results)})
 }, []);
 
   return (
+    // This was for the original PokeAPI
     <div className="App">
       {pokedex.length > 0 && pokedex.map((pokedex, index)=>{
                 return (<div key={index}>{pokedex.name}</div>)
